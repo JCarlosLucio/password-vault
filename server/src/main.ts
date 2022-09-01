d3d14d6bfb6f1d35b7c1853cff1c9d9123622667
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-08-31 23:58:02 -0500

    Add initial fastify server

---
+++ server/src/main.ts
@@ -0,0 +1,36 @@
+import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify';
+
+const server: FastifyInstance = Fastify({});
+
+const opts: RouteShorthandOptions = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          pong: {
+            type: 'string',
+          },
+        },
+      },
+    },
+  },
+};
+
+server.get('/ping', opts, async (request, reply) => {
+  return { pong: 'it worked!' };
+});
+
+const start = async () => {
+  try {
+    await server.listen({ port: 3001 });
+
+    const address = server.server.address();
+    const port = typeof address === 'string' ? address : address?.port;
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
