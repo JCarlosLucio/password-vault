f8df68b87856e740d334d140adc14917d66893e7
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-09-09 23:51:30 -0500

    Add custom eslint config

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,64 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+  env: {
+    browser: true,
+    node: true,
   },
+  extends: [
+    'next',
+    'turbo',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // Make sure this is always the last element in the array.
+  ],
+  plugins: ['simple-import-sort', 'prettier'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: ['apps/*/tsconfig.json'],
+      },
+    },
+  },
+  rules: {
+    // default
+    eqeqeq: 'error',
+    'no-console': 'warn',
+    // react
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+      },
+    ],
+    // next
+    '@next/next/no-html-link-for-pages': 'off',
+    // prettier
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    //typescript
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-namespace': ['error', { allowDeclarations: true }],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    //simple-import-sort
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+  ignorePatterns: [
+    '**/*.js',
+    '**/*.json',
+    'node_modules',
+    'public',
+    'styles',
+    '.next',
+    'coverage',
+    'dist',
+    '.turbo',
+  ],
 };
