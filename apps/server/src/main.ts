52bbed39b5020b97b9c77bf2c172be3e0bcf8f58
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-09-13 22:21:38 -0500

    Connect to Db on server start - Add gracefulShutdown for SIGTEM / SIGINT

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,14 +1,35 @@
+import { FastifyInstance } from 'fastify';
+
+import { PORT } from './constants';
 import createServer from './utils/createServer';
+import { connectToDb, disconnectFromDb } from './utils/db';
 import logger from './utils/logger';
+
+const gracefulShutdown = (signal: string, app: FastifyInstance) => {
+  process.on(signal, async () => {
+    app.close();
+    await disconnectFromDb();
+    logger.info(`Goodbye, got signal ${signal}`);
+    process.exit(0);
+  });
+};
 
 const main = async () => {
   const app = createServer();
+
   try {
-    const url = await app.listen({ port: 4000, host: '0.0.0.0' });
+    const url = await app.listen({ port: PORT, host: '0.0.0.0' });
+    await connectToDb();
     logger.info(`Server is ready at ${url}`);
   } catch (err) {
     logger.error(err);
     process.exit(1);
+  }
+
+  const signals = ['SIGTERM', 'SIGINT'];
+
+  for (const signal of signals) {
+    gracefulShutdown(signal, app);
   }
 };
 
