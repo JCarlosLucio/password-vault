b3832ff27ccab4e40946c8e0b6f6d522e6b487fc
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-09-13 22:34:45 -0500

    Add CORS_ORIGIN constant

--- apps/server/src/constants.ts
+++ apps/server/src/constants.ts
@@ -5,3 +5,5 @@ dotenv.config({ path: '../../.env' });
 export const PORT = Number(process.env.PORT) || 4000;
 
 export const DATABASE_URL = process.env.DATABASE_URL || '';
+
+export const CORS_ORIGIN = process.env.CORS_ORIGIN || 'http://localhost:3000';
