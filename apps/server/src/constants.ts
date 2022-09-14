677d9633bc8ff9a1e6e2335191d7fe32e2d06176
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-09-13 22:17:26 -0500

    Add DATABASE_URL to constants

--- apps/server/src/constants.ts
+++ apps/server/src/constants.ts
@@ -3,3 +3,5 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: '../../.env' });
 
 export const PORT = Number(process.env.PORT) || 4000;
+
+export const DATABASE_URL = process.env.DATABASE_URL || '';
