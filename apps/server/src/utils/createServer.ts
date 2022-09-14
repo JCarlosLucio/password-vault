906fec38b0e82c821daf646fd4e4e8ee48183377
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-09-13 22:35:33 -0500

    Register cors fastify plugin

--- apps/server/src/utils/createServer.ts
+++ apps/server/src/utils/createServer.ts
@@ -1,7 +1,15 @@
+import cors from '@fastify/cors';
 import Fastify, { FastifyInstance } from 'fastify';
+
+import { CORS_ORIGIN } from '../constants';
 
 const createServer = () => {
   const app: FastifyInstance = Fastify();
+
+  app.register(cors, {
+    origin: CORS_ORIGIN,
+    credentials: true,
+  });
 
   return app;
 };
