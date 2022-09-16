02947468e329f2fc88c3f49d06df375e18f717ac
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-09-15 23:19:12 -0500

    Setup app to use jwt

--- apps/server/src/utils/createServer.ts
+++ apps/server/src/utils/createServer.ts
@@ -1,5 +1,8 @@
 import cors from '@fastify/cors';
+import jwt from '@fastify/jwt';
 import Fastify, { FastifyInstance } from 'fastify';
+import { readFileSync } from 'fs';
+import path from 'path';
 
 import { CORS_ORIGIN } from '../constants';
 
@@ -9,6 +12,24 @@ const createServer = () => {
   app.register(cors, {
     origin: CORS_ORIGIN,
     credentials: true,
+  });
+
+  app.register(jwt, {
+    secret: {
+      private: readFileSync(
+        `${path.join(process.cwd(), 'certs')}/private.key`,
+        'utf8',
+      ),
+      public: readFileSync(
+        `${path.join(process.cwd(), 'certs')}/public.key`,
+        'utf8',
+      ),
+    },
+    sign: { algorithm: 'RS256' },
+    cookie: {
+      cookieName: 'token',
+      signed: false,
+    },
   });
 
   return app;
