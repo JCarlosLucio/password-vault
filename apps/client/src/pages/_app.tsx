f6ab067cbaf0d19d12eda16215dc11d344ab93b2
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-09-17 22:05:51 -0500

    Fix path to styles

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../../styles/globals.css';
+import '../styles/globals.css';
 
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
