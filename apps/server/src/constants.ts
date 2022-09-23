424e3d0c47a92c0bc3855ab10f9f18cb165df378
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-09-22 23:53:41 -0500

    Add COOKIE_DOMAIN

--- apps/server/src/constants.ts
+++ apps/server/src/constants.ts
@@ -7,3 +7,5 @@ export const PORT = Number(process.env.PORT) || 4000;
 export const DATABASE_URL = process.env.DATABASE_URL || '';
 
 export const CORS_ORIGIN = process.env.CORS_ORIGIN || 'http://localhost:3000';
+
+export const COOKIE_DOMAIN = process.env.COOKIE_DOMAIN || 'localhost';
