20df0d11631729bdc99ff4e75a2fa63b3a453ef3
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-09-22 23:54:40 -0500

    Add IS_PRODUCTION

--- apps/server/src/constants.ts
+++ apps/server/src/constants.ts
@@ -9,3 +9,5 @@ export const DATABASE_URL = process.env.DATABASE_URL || '';
 export const CORS_ORIGIN = process.env.CORS_ORIGIN || 'http://localhost:3000';
 
 export const COOKIE_DOMAIN = process.env.COOKIE_DOMAIN || 'localhost';
+
+export const IS_PRODUCTION = process.env.NODE_ENV === 'production' || false;
