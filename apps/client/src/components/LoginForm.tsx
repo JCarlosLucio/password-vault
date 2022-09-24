f4a8d6b747cd0c0d2b2dc066aea16a6a967e3700
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-09-23 23:55:09 -0500

    Add email/password inputs

--- apps/client/src/components/LoginForm.tsx
+++ apps/client/src/components/LoginForm.tsx
@@ -1,5 +1,31 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+} from '@chakra-ui/react';
+
+import FormWrapper from './FormWrapper';
+
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  return (
+    <FormWrapper>
+      <Heading>Login</Heading>
+
+      <FormControl mt="4">
+        <FormLabel htmlFor="email">Email</FormLabel>
+        <Input id="email" placeholder="Email" />
+      </FormControl>
+
+      <FormControl mt="4">
+        <FormLabel htmlFor="password">Password</FormLabel>
+        <Input id="password" placeholder="Password" type="password" />
+      </FormControl>
+
+      <Button type="submit">Login</Button>
+    </FormWrapper>
+  );
 };
 
 export default LoginForm;
