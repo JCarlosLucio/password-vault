652b4dc1dbaf813b9072a33a11f605f5e35c5632
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-09-23 23:19:52 -0500

    Add Box to FormWrapper

--- apps/client/src/components/FormWrapper.tsx
+++ apps/client/src/components/FormWrapper.tsx
@@ -1,5 +1,15 @@
-const FormWrapper = () => {
-  return <div>FormWrapper</div>;
+import { Box, BoxProps } from '@chakra-ui/react';
+import { ReactNode } from 'react';
+
+const FormWrapper = ({
+  children,
+  ...props
+}: { children: ReactNode } & BoxProps) => {
+  return (
+    <Box w="100%" maxW="container.sm" boxShadow="xl" p="8" as="form" {...props}>
+      {children}
+    </Box>
+  );
 };
 
 export default FormWrapper;
