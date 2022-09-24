cf4392a4d769fd362040849b66127a4e3ded73dd
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-09-23 23:51:32 -0500

    Add theme for ChakraUI

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,15 +1,22 @@
 import '../styles/globals.css';
 
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 
 const queryClient = new QueryClient();
 
+const theme = extendTheme({
+  config: {
+    useSystemColorMode: true,
+    initialColorMode: 'dark',
+  },
+});
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Component {...pageProps} />;
       </ChakraProvider>
     </QueryClientProvider>
