6184c856d68394cbe117ef7e1e5cdc158b38e8cb
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-09-25 00:18:16 -0500

    Add storeVault - Rename VAULT_STORAGE_KEY to VAULT_KEY_STORAGE_KEY - Add VAULT_STORAGE_KEY to constants

--- apps/client/src/utils/storage.ts
+++ apps/client/src/utils/storage.ts
@@ -1,5 +1,9 @@
-import { VAULT_STORAGE_KEY } from './constants';
+import { VAULT_KEY_STORAGE_KEY, VAULT_STORAGE_KEY } from './constants';
 
 export const storeVaultKey = (vaultKey: string) => {
-  window.sessionStorage.setItem(VAULT_STORAGE_KEY, vaultKey);
+  window.sessionStorage.setItem(VAULT_KEY_STORAGE_KEY, vaultKey);
+};
+
+export const storeVault = (vault: string) => {
+  window.sessionStorage.setItem(VAULT_STORAGE_KEY, vault);
 };
