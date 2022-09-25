afe3ef70512843122195216ea9b5425e8f213f31
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-09-24 23:30:17 -0500

    Setup react-hook-form for the RegisterForm

--- apps/client/src/components/RegisterForm.tsx
+++ apps/client/src/components/RegisterForm.tsx
@@ -1,29 +1,78 @@
 import {
   Button,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Heading,
   Input,
 } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
 
+import { hashPassword } from '../utils/crypto';
 import FormWrapper from './FormWrapper';
 
 const RegisterForm = () => {
+  const {
+    handleSubmit,
+    register,
+    getValues,
+    setValue,
+    formState: { errors, isSubmitting },
+  } = useForm<{ email: string; password: string; hashedPassword: string }>();
+
   return (
-    <FormWrapper>
+    <FormWrapper
+      onSubmit={handleSubmit(() => {
+        const password = getValues('password');
+        const hashedPassword = hashPassword(password);
+
+        setValue('hashedPassword', hashedPassword);
+      })}
+    >
       <Heading>Register</Heading>
 
       <FormControl mt="4">
         <FormLabel htmlFor="email">Email</FormLabel>
-        <Input id="email" placeholder="Email" />
+        <Input
+          id="email"
+          placeholder="Email"
+          {...register('email', {
+            required: 'Email is required',
+            minLength: {
+              value: 4,
+              message: 'Email must be at least 4 characters long',
+            },
+          })}
+        />
+
+        <FormErrorMessage>
+          {errors.email && errors.email.message}
+        </FormErrorMessage>
       </FormControl>
 
       <FormControl mt="4">
         <FormLabel htmlFor="password">Password</FormLabel>
-        <Input id="password" placeholder="Password" type="password" />
+        <Input
+          id="password"
+          placeholder="Password"
+          type="password"
+          {...register('password', {
+            required: 'Password is required',
+            minLength: {
+              value: 6,
+              message: 'Password must be at least 6 characters long',
+            },
+          })}
+        />
+
+        <FormErrorMessage>
+          {errors.password && errors.password.message}
+        </FormErrorMessage>
       </FormControl>
 
-      <Button type="submit">Register</Button>
+      <Button type="submit" isLoading={isSubmitting}>
+        Register
+      </Button>
     </FormWrapper>
   );
 };
