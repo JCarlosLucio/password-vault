0c88940554570758f81b12df8436c85e94ff06c7
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-09-29 22:29:36 -0500

    Fix createUser to take correct arguments

--- apps/server/src/modules/user/user.service.ts
+++ apps/server/src/modules/user/user.service.ts
@@ -6,9 +6,9 @@ export const generateSalt = () => {
   return crypto.randomBytes(64).toString('hex');
 };
 
-export async function createUser(input: {
-  hashedPassword: string;
-  email: string;
-}) {
-  return UserModel.create(input);
+export function createUser(input: { hashedPassword: string; email: string }) {
+  return UserModel.create({
+    email: input.email,
+    password: input.hashedPassword,
+  });
 }
