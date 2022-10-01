bbc8f4fa2145a830280967b6d9bfcf3c996bfeba
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-09-30 23:12:28 -0500

    Add updateVault

--- apps/server/src/modules/vault/vault.service.ts
+++ apps/server/src/modules/vault/vault.service.ts
@@ -3,3 +3,13 @@ import { VaultModel } from './vault.model';
 export function createVault(input: { user: string; salt: string }) {
   return VaultModel.create(input);
 }
+
+export const updateVault = ({
+  userId,
+  data,
+}: {
+  userId: string;
+  data: string;
+}) => {
+  return VaultModel.updateOne({ user: userId }, { data });
+};
