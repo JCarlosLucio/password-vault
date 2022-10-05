575ec700db01662aa5abf0aa2512418454d6de64
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-10-05 18:52:54 -0500

    Add saveVault

--- apps/client/src/api/index.ts
+++ apps/client/src/api/index.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { API_URL } from '../utils/constants';
 
 const userBase = `${API_URL}/api/users`;
+const vaultBase = `${API_URL}/api/vault`;
 
 export const registerUser = async (payload: {
   hashedPassword: string;
@@ -11,6 +12,22 @@ export const registerUser = async (payload: {
   const response = await axios.post<{ salt: string; vault: string }>(
     userBase,
     payload,
+    {
+      withCredentials: true,
+    },
+  );
+
+  return response.data;
+};
+
+export const saveVault = async ({
+  encryptedVault,
+}: {
+  encryptedVault: string;
+}) => {
+  const response = await axios.put(
+    vaultBase,
+    { encryptedVault },
     {
       withCredentials: true,
     },
