8840a587e185447af3cc315b5c14daef5e2f6f82
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-10-06 17:35:24 -0500

    Add findUserByEmailAndPassword - Add genHash

--- apps/server/src/modules/user/user.service.ts
+++ apps/server/src/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import * as argon2 from 'argon2';
 import crypto from 'crypto';
 
 import { UserModel } from './user.model';
@@ -11,4 +12,26 @@ export function createUser(input: { hashedPassword: string; email: string }) {
     email: input.email,
     password: input.hashedPassword,
   });
+}
+
+const genHash = (password: string) => {
+  return argon2.hash(password);
+};
+
+export async function findUserByEmailAndPassword({
+  email,
+  hashedPassword,
+}: {
+  email: string;
+  hashedPassword: string;
+}) {
+  const user = await UserModel.findOne({ email });
+
+  const hash = await genHash(hashedPassword);
+
+  if (!user || !argon2.verify(user.password, hash)) {
+    return null;
+  }
+
+  return user;
 }
