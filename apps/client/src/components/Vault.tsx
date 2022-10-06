7acf261ac6e340a8a1c96faa97a2da3e9d032842
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-10-05 19:19:17 -0500

    Add defaultValues for Form

--- apps/client/src/components/Vault.tsx
+++ apps/client/src/components/Vault.tsx
@@ -15,7 +15,11 @@ const Vault = ({
   vault: VaultItem[];
   vaultKey: string;
 }) => {
-  const { control, register, handleSubmit } = useForm();
+  const { control, register, handleSubmit } = useForm({
+    defaultValues: {
+      vault,
+    },
+  });
   const { fields, append, remove } = useFieldArray({
     control,
     name: 'vault',
