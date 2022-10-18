3c081ea30bec6fd5090f19d45407ed9ff8b7d851
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-10-17 23:17:47 -0500

    Update DATABASE_URL constant

--- apps/server/src/constants.ts
+++ apps/server/src/constants.ts
@@ -4,10 +4,12 @@ dotenv.config();
 
 export const PORT = Number(process.env.PORT) || 4000;
 
-export const DATABASE_URL = process.env.DATABASE_URL || '';
-
 export const CORS_ORIGIN = process.env.CORS_ORIGIN || 'http://localhost:3000';
 
 export const COOKIE_DOMAIN = process.env.COOKIE_DOMAIN || 'localhost';
 
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production' || false;
+
+export const DATABASE_URL =
+  (IS_PRODUCTION ? process.env.DATABASE_URL : process.env.TEST_DATABASE_URL) ||
+  '';
