e6668362cb1033466bac818e31a0cd8b2d11392b
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-10-20 18:46:27 -0500

    Test should register user with valid email and password

--- e2e/password-vault.spec.ts
+++ e2e/password-vault.spec.ts
@@ -4,9 +4,23 @@ test.beforeEach(async ({ page }) => {
   await page.goto('/'); // http://localhost:3000
 });
 
+const TEST_USER = {
+  email: 'e2e@test.com',
+  password: 'hashedPassword',
+};
+
 test.describe('Register', () => {
   test('should show Password Vault in title and Register', async ({ page }) => {
     await expect(page).toHaveTitle(/Password Vault/);
     await expect(page.getByRole('heading')).toHaveText('Register');
+  });
+
+  test('should register user with valid email and password', async ({
+    page,
+  }) => {
+    await page.locator('data-test=email-input').fill(TEST_USER.email);
+    await page.locator('data-test=password-input').fill(TEST_USER.password);
+    await page.locator('data-test=register-btn').click();
+    await expect(page.getByRole('heading')).toHaveText('Vault');
   });
 });
