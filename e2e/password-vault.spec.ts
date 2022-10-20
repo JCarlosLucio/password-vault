b149761e3e508c842567a960cd0ce3db982640d5
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-10-20 17:16:31 -0500

    Refactor e2e tests

--- e2e/password-vault.spec.ts
+++ e2e/password-vault.spec.ts
@@ -1,8 +1,13 @@
 import { expect, test } from '@playwright/test';
 
-test('homepage has Password Vault in title', async ({ page }) => {
-  await page.goto('/');
+test.beforeEach(async ({ page }) => {
+  await page.goto('/'); // http://localhost:3000
+});
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Password Vault/);
+test.describe('Login', () => {
+  test('should show Password Vault in title and Login', async ({ page }) => {
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Password Vault/);
+    expect(page.getByText('Login')).toBeDefined();
+  });
 });
