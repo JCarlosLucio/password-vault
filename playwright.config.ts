de1d881f021ce17d3eda7894f89c43aa015dd4ea
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-10-20 19:35:16 -0500

    Change webServer command to yarn start:test

--- playwright.config.ts
+++ playwright.config.ts
@@ -105,7 +105,7 @@ const config: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn dev',
+    command: 'yarn start:test',
     port: 3000,
     reuseExistingServer: !process.env.CI,
   },
