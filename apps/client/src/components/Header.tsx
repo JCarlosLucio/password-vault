24d5c3c2a13ac296a5ae4b7873f5a27b4713015a
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-10-31 23:15:48 -0600

    Add Header component

---
+++ apps/client/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { Heading } from '@chakra-ui/react';
+
+const Header = () => {
+  return (
+    <header>
+      <Heading as="h1" size="3xl">
+        Password Vault
+      </Heading>
+    </header>
+  );
+};
+
+export default Header;
