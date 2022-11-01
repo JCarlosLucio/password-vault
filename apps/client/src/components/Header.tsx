5b66223c52f59bdc452e419b41d75a0689da6314
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-10-31 23:53:20 -0600

    Add Shield/Key Icon

--- apps/client/src/components/Header.tsx
+++ apps/client/src/components/Header.tsx
@@ -1,12 +1,23 @@
-import { Heading } from '@chakra-ui/react';
+import { Flex, Heading, Icon } from '@chakra-ui/react';
+import { RiShieldKeyholeFill } from 'react-icons/ri';
 
 const Header = () => {
   return (
-    <header>
-      <Heading as="h1" size="3xl">
+    <Flex
+      as="header"
+      justify="center"
+      align="center"
+      direction={['column', 'row']}
+      mt={[10, 40]}
+      mb={[1, 20]}
+      mx="10"
+      gap={[1, 5]}
+    >
+      <Icon as={RiShieldKeyholeFill} boxSize={[14, 16]} />
+      <Heading as="h1" size={['2xl', '3xl']} textAlign="center">
         Password Vault
       </Heading>
-    </header>
+    </Flex>
   );
 };
 
