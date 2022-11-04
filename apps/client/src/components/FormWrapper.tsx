09b93ec49917e3af5a7e643ef86c165b5d897c82
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-11-03 23:13:59 -0600

    Animate FormWrapper from side to center

--- apps/client/src/components/FormWrapper.tsx
+++ apps/client/src/components/FormWrapper.tsx
@@ -1,15 +1,25 @@
 import { Box, BoxProps } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
 import { ReactNode } from 'react';
 
-type FormWrapperProps = { children: ReactNode } & BoxProps;
+type FormWrapperProps = { children: ReactNode; initialX?: number } & BoxProps;
 
 const FormWrapper = ({
   children,
   maxW = 'container.sm',
+  initialX = -100,
   ...props
 }: FormWrapperProps) => {
   return (
-    <Box w="100%" maxW={maxW} p="8" as="form" {...props}>
+    <Box
+      w="100%"
+      maxW={maxW}
+      p="8"
+      as={motion.form}
+      initial={{ opacity: 0, x: initialX }}
+      animate={{ opacity: 1, x: 0 }}
+      {...props}
+    >
       {children}
     </Box>
   );
