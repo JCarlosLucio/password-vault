b42d0df5cd61c873f4845ddc33329189f75f7b6a
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-11-03 22:52:17 -0600

    Refactor motion into Flex component - Remove motion.div

--- apps/client/src/components/Header.tsx
+++ apps/client/src/components/Header.tsx
@@ -5,32 +5,29 @@ import ShieldIcon from './ShieldIcon';
 
 const Header = () => {
   return (
-    <motion.div
+    <Flex
+      justify="center"
+      align="center"
+      direction={['column', 'row']}
+      mt={[10, 40]}
+      mb={[1, 20]}
+      mx="10"
+      gap={[1, 5]}
+      as={motion.header}
       initial={{ opacity: 0, translateY: -100 }}
       animate={{ opacity: 1, translateY: 0 }}
     >
-      <Flex
-        as="header"
-        justify="center"
-        align="center"
-        direction={['column', 'row']}
-        mt={[10, 40]}
-        mb={[1, 20]}
-        mx="10"
-        gap={[1, 5]}
+      <ShieldIcon boxSize={[14, 16]} />
+      <Heading
+        as="h1"
+        size={['2xl', '3xl']}
+        textAlign="center"
+        fontSize="6xl"
+        variant="gradient"
       >
-        <ShieldIcon boxSize={[14, 16]} />
-        <Heading
-          as="h1"
-          size={['2xl', '3xl']}
-          textAlign="center"
-          fontSize="6xl"
-          variant="gradient"
-        >
-          Password Vault
-        </Heading>
-      </Flex>
-    </motion.div>
+        Password Vault
+      </Heading>
+    </Flex>
   );
 };
 
