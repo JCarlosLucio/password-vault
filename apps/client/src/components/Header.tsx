6b3ec5d72758722325ab7323c82694e735718d42
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-11-03 22:43:06 -0600

    Animate Header entrance

--- apps/client/src/components/Header.tsx
+++ apps/client/src/components/Header.tsx
@@ -1,31 +1,36 @@
 import { Flex, Heading } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
 
 import ShieldIcon from './ShieldIcon';
 
 const Header = () => {
   return (
-    <Flex
-      as="header"
-      justify="center"
-      align="center"
-      direction={['column', 'row']}
-      mt={[10, 40]}
-      mb={[1, 20]}
-      mx="10"
-      gap={[1, 5]}
+    <motion.div
+      initial={{ opacity: 0, translateY: -100 }}
+      animate={{ opacity: 1, translateY: 0 }}
     >
-      <ShieldIcon boxSize={[14, 16]} />
-
-      <Heading
-        as="h1"
-        size={['2xl', '3xl']}
-        textAlign="center"
-        fontSize="6xl"
-        variant="gradient"
+      <Flex
+        as="header"
+        justify="center"
+        align="center"
+        direction={['column', 'row']}
+        mt={[10, 40]}
+        mb={[1, 20]}
+        mx="10"
+        gap={[1, 5]}
       >
-        Password Vault
-      </Heading>
-    </Flex>
+        <ShieldIcon boxSize={[14, 16]} />
+        <Heading
+          as="h1"
+          size={['2xl', '3xl']}
+          textAlign="center"
+          fontSize="6xl"
+          variant="gradient"
+        >
+          Password Vault
+        </Heading>
+      </Flex>
+    </motion.div>
   );
 };
 
