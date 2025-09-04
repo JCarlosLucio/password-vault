218355d36d80ae259a22a0d477c2f76ccd21b70a
Juan Carlos Lucio <c.lucio3@gmail.com>
2025-09-03 21:15:43 -0600

    Update prettier configs

---
+++ prettier.config.mjs
@@ -0,0 +1,17 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+  useTabs: false,
+  bracketSpacing: true,
+  proseWrap: 'always',
+  endOfLine: 'auto',
+};
+
+export default config;
