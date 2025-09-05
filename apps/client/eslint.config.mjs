71db13597ada33e3985e3b448b49f129eef9707e
Juan Carlos Lucio <c.lucio3@gmail.com>
2025-09-05 00:00:20 -0600

    Migrate eslint config to new flat config - Bump eslint to 9.34.0 in client,server,eslint-config-custom,ui - Update lint scripts - Bump eslint-config-prettier,eslint-plugin-import,eslint-plugin-react,eslint-plugin-simple-import-sort,eslint-plugin-turbo - Replace @typescript-eslint/parser,@typescript-eslint/eslint-plugin with typescript-eslint - Replace eslint-config-next with eslint-plugin-next - Migrate eslint-config-custom/index.js to flat config - Divide eslint-coonfig-custom/index.js into index.js,next.js,react.js - Migrate all .eslintrc.js to eslint.config.mjs for flat config - Add tsconfig.production.json - Update server build script to use tsconfig.production.json - Remove "src/**/*.test.ts" from server tsconfig.json for eslint detection - Remove "**/*.ts","**/*.tsx" from exclude in client tsconfig.json

---
+++ apps/client/eslint.config.mjs
@@ -0,0 +1,12 @@
+import nextConfig from '@repo/eslint-config-custom/next.js';
+import reactConfig from '@repo/eslint-config-custom/react.js';
+import { defineConfig } from 'eslint/config';
+
+/** @type {import("eslint").Linter.Config} */
+export default defineConfig([
+  {
+    files: ['**/*.{ts,tsx}'],
+    name: 'client-config',
+    extends: [nextConfig, reactConfig],
+  },
+]);
---
+++ packages/ui/eslint.config.mjs
@@ -0,0 +1,11 @@
+import reactConfig from '@repo/eslint-config-custom/react.js';
+import { defineConfig } from 'eslint/config';
+
+/** @type {import("eslint").Linter.Config} */
+export default defineConfig([
+  {
+    files: ['**/*.{ts,tsx}'],
+    name: 'ui-config',
+    extends: [reactConfig],
+  },
+]);
