915b3bcbb826f14c98b73d593263e8b77f2d1075
Juan Carlos Lucio <c.lucio3@gmail.com>
2025-09-05 01:21:59 -0600

    Swap optional operator (||) for nullish coalescing (??)

--- apps/server/src/constants.ts
+++ apps/server/src/constants.ts
@@ -5,14 +5,14 @@ dotenv.config();
 
 export const PORT = Number(process.env.PORT) || 4000;
 
-export const CORS_ORIGIN = process.env.CORS_ORIGIN || 'http://localhost:3000';
+export const CORS_ORIGIN = process.env.CORS_ORIGIN ?? 'http://localhost:3000';
 
-export const COOKIE_DOMAIN = process.env.COOKIE_DOMAIN || 'localhost';
+export const COOKIE_DOMAIN = process.env.COOKIE_DOMAIN ?? 'localhost';
 
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production' || false;
 
 export const DATABASE_URL =
-  (IS_PRODUCTION ? process.env.DATABASE_URL : process.env.TEST_DATABASE_URL) ||
+  (IS_PRODUCTION ? process.env.DATABASE_URL : process.env.TEST_DATABASE_URL) ??
   '';
 
 export const IS_TESTING = process.env.NODE_ENV === 'test';
