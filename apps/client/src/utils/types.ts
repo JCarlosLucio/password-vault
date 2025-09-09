b1f3e5a71540854b480536285483bb12de552c57
Juan Carlos Lucio <c.lucio3@gmail.com>
2025-09-08 18:43:07 -0600

    Add type guards for VaultItem/VaultItemArray/VaultData

--- apps/client/src/utils/types.ts
+++ apps/client/src/utils/types.ts
@@ -3,3 +3,64 @@ export interface VaultItem {
   username: string;
   password: string;
 }
+
+export interface VaultData {
+  vault: VaultItem[];
+}
+
+function isString(text: unknown): text is string {
+  return typeof text === 'string' || text instanceof String;
+}
+
+function isVaultItem(obj: unknown): obj is VaultItem {
+  return (
+    typeof obj === 'object' &&
+    obj !== null &&
+    'website' in obj &&
+    'username' in obj &&
+    'password' in obj &&
+    isString(obj.website) &&
+    isString(obj.username) &&
+    isString(obj.password)
+  );
+}
+
+function parseVaultItem(obj: unknown): VaultItem {
+  if (!isVaultItem(obj)) {
+    throw new Error('Incorrect or missing vault item');
+  }
+  return obj;
+}
+
+function isVaultItemArray(obj: unknown): obj is VaultItem[] {
+  return (
+    typeof obj === 'object' &&
+    obj !== null &&
+    Array.isArray(obj) &&
+    obj.every(parseVaultItem)
+  );
+}
+
+export function parseVaultItemArray(obj: unknown): VaultItem[] {
+  if (!isVaultItemArray(obj)) {
+    throw new Error('Incorrect or missing vault items');
+  }
+  return obj;
+}
+
+function isVaultData(obj: unknown): obj is VaultData {
+  return (
+    typeof obj === 'object' &&
+    obj !== null &&
+    'vault' in obj &&
+    Array.isArray(obj.vault) &&
+    obj.vault.every(parseVaultItem)
+  );
+}
+
+export function parseVaultData(obj: unknown): VaultData {
+  if (!isVaultData(obj)) {
+    throw new Error('Incorrect or missing vault');
+  }
+  return obj;
+}
