fa4f81ae59cf65782234876e87ff508e290f5465
Juan Carlos Lucio <c.lucio3@gmail.com>
2025-09-17 14:52:35 -0600

    Migrate theme to Chakra v3

---
+++ apps/client/src/utils/theme.ts
@@ -0,0 +1,71 @@
+import { createSystem, defaultConfig, defineRecipe } from '@chakra-ui/react';
+
+export const system = createSystem(defaultConfig, {
+  theme: {
+    tokens: {
+      fonts: {
+        body: {
+          value:
+            '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,sans-serif',
+        },
+      },
+      lineHeights: {
+        normal: { value: '1.5' },
+      },
+    },
+    recipes: {
+      heading: defineRecipe({
+        variants: {
+          variant: {
+            gradient: {
+              fontWeight: 'extrabold',
+              bgClip: 'text',
+              bgGradient: 'to-r',
+              gradientFrom: 'orange.300',
+              gradientTo: 'red.500',
+              _dark: {
+                gradientFrom: 'yellow.400',
+                gradientTo: 'orange.500',
+              },
+            },
+          },
+        },
+      }),
+      button: defineRecipe({
+        variants: {
+          variant: {
+            gradient: {
+              transition: '500ms',
+              color: 'black',
+              bgSize: '200% 100%',
+              bgGradient: 'to-r',
+              gradientFrom: 'orange.400',
+              gradientVia: 'red.500',
+              gradientTo: 'orange.300',
+              _dark: {
+                gradientFrom: 'yellow.400',
+                gradientVia: 'orange.500',
+                gradientTo: 'yellow.400',
+              },
+              _hover: {
+                backgroundPosition: 'right center',
+                _disabled: {
+                  backgroundPosition: 'right center',
+                  bgSize: '200% 100%',
+                  gradientFrom: 'orange.400',
+                  gradientVia: 'red.500',
+                  gradientTo: 'orange.300',
+                  _dark: {
+                    gradientFrom: 'yellow.400',
+                    gradientVia: 'orange.500',
+                    gradientTo: 'yellow.400',
+                  },
+                },
+              },
+            },
+          },
+        },
+      }),
+    },
+  },
+});
