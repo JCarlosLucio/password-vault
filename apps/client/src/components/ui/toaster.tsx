9bcc5771015b35558e8e17df4869c8a634459580
Juan Carlos Lucio <c.lucio3@gmail.com>
2025-09-18 16:56:00 -0600

    Add data-testid toast,toast-title,toast-description

--- apps/client/src/components/ui/toaster.tsx
+++ apps/client/src/components/ui/toaster.tsx
@@ -20,16 +20,22 @@ export const Toaster = () => {
     <Portal>
       <ChakraToaster toaster={toaster} insetInline={{ mdDown: '4' }}>
         {(toast) => (
-          <Toast.Root width={{ md: 'sm' }}>
+          <Toast.Root width={{ md: 'sm' }} data-testid="toast">
             {toast.type === 'loading' ? (
               <Spinner size="sm" color="blue.solid" />
             ) : (
               <Toast.Indicator />
             )}
             <Stack gap="1" flex="1" maxWidth="100%">
-              {toast.title && <Toast.Title>{toast.title}</Toast.Title>}
+              {toast.title && (
+                <Toast.Title data-testid="toast-title">
+                  {toast.title}
+                </Toast.Title>
+              )}
               {toast.description && (
-                <Toast.Description>{toast.description}</Toast.Description>
+                <Toast.Description data-testid="toast-description">
+                  {toast.description}
+                </Toast.Description>
               )}
             </Stack>
             {toast.action && (
